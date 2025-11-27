dbe13e0777187ad21990639edd1f5e599b1ed62e
mubashir111 <muba4shir@gmail.com>
2025-11-27 16:16:14 +0300

    set

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { DataProvider } from './context/DataContext';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
 import { AdminDashboard } from './pages/Admin';
+import "./index.css";
+
 
 const App: React.FC = () => {
   return (
